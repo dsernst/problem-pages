18639646ed49c7feb0374c40e684f5806f7ba59b
David Ernst <git@dsernst.com>
2015-09-22 23:10:24 -0700

    Add cool ascii faces

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+var cool = require('cool-ascii-faces')
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function(req, res) {
   res.end('hello world')
 })
 
+app.get('/cool', function(req, res) {
+  res.end(cool())
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'))
 })
-
-
